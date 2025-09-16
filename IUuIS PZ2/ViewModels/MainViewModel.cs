20019d5956ec15063a6b3cc740275224199cea25
goraan03 <gorangrcic72@gmail.com>
2025-09-17 01:16:48 +0200

    File creating

---
+++ IUuIS PZ2/ViewModels/MainViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IUuIS_PZ2.ViewModels
+{
+    internal class MainViewModel
+    {
+    }
+}
